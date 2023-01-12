bad081a57a6ed752429ed030d2eadaab8b3b8135
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-01-12 20:54:17 +0100

    added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod game_utils;
+use crate::game_utils::{NUM_ROWS, NUM_COLS};
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to space invaders Rust version");
+    println!("Number of rows: {}", NUM_ROWS);
+    println!("Number of columns: {}", NUM_COLS);
+
 }
