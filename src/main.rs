2959dd2dcb46fa0a9580b69f01cea854a5f2e937
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-01-12 21:03:51 +0100

    adding basic termlnal command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 mod game_utils;
-use crate::game_utils::{NUM_ROWS, NUM_COLS};
+use std::{error::Error, io::stdout};
 
+use crossterm::{terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, cursor::{Hide, Show}, ExecutableCommand};
 
-fn main() {
+use crate::game_utils::{NUM_COLS, NUM_ROWS};
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Welcome to space invaders Rust version");
     println!("Number of rows: {}", NUM_ROWS);
     println!("Number of columns: {}", NUM_COLS);
 
+    // As we will need access to the terminal we need to instantiate access to it with the io module
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
