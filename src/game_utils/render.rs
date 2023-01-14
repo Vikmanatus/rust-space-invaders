d535901ce15feb24c5922c576294e836e0a7ce4a
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-01-14 17:54:06 +0100

    added vscode debugger support

--- src/game_utils/render.rs
+++ src/game_utils/render.rs
@@ -9,13 +9,15 @@
 
 use std::io::{Stdout, Write};
 
-use crossterm::{QueueableCommand, style::{SetBackgroundColor, Color}, terminal::{Clear, ClearType}, ExecutableCommand};
+use crossterm::{QueueableCommand, style::{SetBackgroundColor, Color}, terminal::{Clear, ClearType,}, ExecutableCommand};
 
 // So as for the game rendering,  we will need to have access to a terminal, in that way we will be able to customize the screen
 pub fn render_welcome_screen(stdout: &mut Stdout) {
-    stdout.queue(SetBackgroundColor(Color::Cyan)).unwrap();
-    stdout.queue(Clear(ClearType::All)).unwrap();
-    stdout.flush().unwrap();
+    // stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+    // stdout.queue(Clear(ClearType::All)).unwrap();
+    // stdout.flush().unwrap();
+    stdout.write(b"Some value in terminal").unwrap();
+    // stdout.write(b"Welcome to rust-space-invaders").unwrap();
 }
 
 // To render we need to have an access to the terminal
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crossterm::{
 };
 use rusty_audio::Audio;
 
-use crate::game_utils::{NUM_COLS, NUM_ROWS, add_sounds, render::render_welcome_screen,};
+use crate::game_utils::{add_sounds, render::render_welcome_screen, NUM_COLS, NUM_ROWS};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Welcome to space invaders Rust version");
@@ -26,11 +26,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Hiding content of users terminal and providing a similar result as a vim editor in which we can draw and render the elements of the game
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
+
     audio.play("welcome");
-    
+
+
+
     // We will need a game loop in which we can create the elements of the game
     'gameloop: loop {
-        render_welcome_screen(&mut stdout);
         while poll(Duration::default())? {
             if let Event::Key(key_code) = read()? {
                 match key_code.code {
@@ -42,9 +44,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-    
-    // audio.wait(); // Block until sounds welcome sound finishes playing
 
+    // audio.wait(); // Block until sounds welcome sound finishes playing
 
     // Killing the app and terminating the program
     // Need to create a render engine so we can draw elements into the terminal
