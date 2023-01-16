af848e4090fd3de1f35496f1de86e77edc10a05f
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-01-16 20:45:17 +0100

    WIP: cleaning

--- src/game_utils/render.rs
+++ src/game_utils/render.rs
@@ -16,22 +16,19 @@ use crossterm::{
     ExecutableCommand, QueueableCommand,
 };
 
-use crate::styles::{get_terminal_dimensions, render_background_color, write_text_in_terminal};
+use crate::styles::{
+    get_terminal_dimensions, render_background_color, write_menu_options, write_text_in_terminal,
+};
 
 // So as for the game rendering,  we will need to have access to a terminal, in that way we will be able to customize the screen
 pub fn render_welcome_screen(stdout: &mut Stdout) {
-    // stdout.execute(SetBackgroundColor(Color::Blue)).unwrap();
-    // stdout.execute(Clear(ClearType::All)).unwrap();
     render_background_color(stdout, true);
     let dimnesions = get_terminal_dimensions();
     let welcome_text = b"Welcome to space-invaders rust";
+    let menu_items = ["Play game", "Options", "Quit game"];
+
     write_text_in_terminal(stdout, dimnesions.0 / 2, dimnesions.1 / 10, welcome_text);
-    stdout.execute(SetBackgroundColor(Color::White)).unwrap();
-    write_text_in_terminal(stdout, dimnesions.0 / 4, dimnesions.1 / 8, b"Play game");
-    render_background_color(stdout, false);
-    write_text_in_terminal(stdout, dimnesions.0 / 4, dimnesions.1 / 4, b"Options");
-    write_text_in_terminal(stdout, dimnesions.0 / 4, dimnesions.1 / 3, b"Quit game");
-    // To get the center we need to devide the values by 2
+    write_menu_options(stdout, menu_items, dimnesions.0 / 4, dimnesions.1 / 8);
     println!("\n");
     // stdout.write(b"Welcome to rust-space-invaders").unwrap();
 }
--- src/styles/mod.rs
+++ src/styles/mod.rs
@@ -1,7 +1,7 @@
 use std::io::{Stdout, Write};
 
 use crossterm::{
-    cursor::MoveTo,
+    cursor::{MoveTo, MoveToNextLine},
     style::{Color, SetBackgroundColor},
     terminal::{size, Clear, ClearType},
     ExecutableCommand,
@@ -16,7 +16,25 @@ pub fn render_background_color(stdout: &mut Stdout, clear: bool) {
 pub fn get_terminal_dimensions() -> (u16, u16) {
     size().unwrap()
 }
-
+pub fn write_menu_options(
+    stdout: &mut Stdout,
+    menu_options: [&str; 3],
+    initial_x: u16,
+    initial_y: u16,
+) {
+    stdout.execute(MoveTo(0, initial_y)).unwrap();
+    let mut loop_number = 0;
+    for item in menu_options {
+        loop_number += 1;
+        if loop_number == 1 {
+            stdout.execute(SetBackgroundColor(Color::White)).unwrap();
+        } else {
+            stdout.execute(SetBackgroundColor(Color::Blue)).unwrap();
+        }
+        stdout.write_all(item.as_bytes()).unwrap();
+        stdout.execute(MoveToNextLine(1)).unwrap();
+    }
+}
 pub fn write_text_in_terminal(stdout: &mut Stdout, x_position: u16, y_position: u16, buf: &[u8]) {
     stdout.execute(MoveTo(x_position, y_position)).unwrap();
     stdout.write_all(buf).unwrap();
