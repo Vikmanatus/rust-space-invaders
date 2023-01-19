b9720a3e9b9278c33c57111678845d7a12762884
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-01-19 01:17:32 +0100

    game launching from main menu

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 mod game_utils;
 mod styles;
-use std::{error::Error, io::{stdout, self}, time::Duration, sync::mpsc, thread::{self, sleep}};
+use std::{
+    error::Error,
+    io::{ stdout},
+    sync::mpsc,
+    thread::{ sleep},
+    time::Duration,
+};
 
 use crossterm::{
     cursor::{Hide, Show},
     event::{poll, read, Event, KeyCode},
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen, Clear, ClearType},
+    terminal::{self,  EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
 use rusty_audio::Audio;
 
 use crate::{
     game_utils::{
-        add_sounds, render::{render_welcome_screen, render}, MenuResetRequired, MENU_ITEMS, NUM_COLS,
-        NUM_ROWS, frame::{self, new_frame},
+        add_sounds,
+        frame::{ new_frame},
+        lauch_game_thread,
+        render::{ render_welcome_screen},
+        MenuResetRequired, MENU_ITEMS, NUM_COLS, NUM_ROWS,
     },
     styles::style_menu_index,
 };
@@ -38,28 +47,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut current_menu_index = 0;
     style_menu_index(&mut stdout, current_menu_index, MenuResetRequired::None);
-
-    // Render loop in a separate thread for speedup
-    let (render_tx, render_rx) = mpsc::channel();
-    let render_handler = thread::spawn(move || {
-        let mut last_frame = frame::new_frame();
-        let mut stdout = io::stdout();
-        render(&mut stdout, &last_frame, &last_frame, true);
-        loop {
-            let curr_frame = match render_rx.recv() {
-                Ok(x) => x,
-                Err(_) => break,
-            };
-            render(&mut stdout, &last_frame, &curr_frame, false);
-            last_frame = curr_frame;
-        }
-    });
-
-
+    let mut game_quit_required = false;
     // We will need a game loop in which we can create the elements of the game
-    'gameloop: loop {
-        // Per-frame init
-        let curr_frame = new_frame();
+    'menu_control_loop: loop {
         while poll(Duration::default())? {
             if let Event::Key(key_code) = read()? {
                 match key_code.code {
@@ -86,28 +76,50 @@ fn main() -> Result<(), Box<dyn Error>> {
                     }
                     KeyCode::Enter => match current_menu_index {
                         0 => {
-
+                            audio.play("play");
+                            audio.wait();
+                            break 'menu_control_loop;
                         }
                         1 => {
+                            game_quit_required = true;
                             play_goodbye_song(&mut audio);
-                            break 'gameloop;
+                            break 'menu_control_loop;
                         }
                         _ => {}
                     },
-                    KeyCode::Esc => {
-                        play_goodbye_song(&mut audio);
-                        break 'gameloop;
-                    }
                     _ => {}
                 }
             }
         }
-        // Draw & render
-        let _ = render_tx.send(curr_frame);
-        sleep(Duration::from_millis(1));
     }
-    drop(render_tx);
-    render_handler.join().unwrap();
+    if !game_quit_required {
+        // Render loop in a separate thread for speedup
+        let (render_tx, render_rx) = mpsc::channel();
+        let render_handler = lauch_game_thread(render_rx);
+        audio.play("start_game");
+        audio.wait();
+        'gameloop: loop {
+            // Per-frame init
+            let curr_frame = new_frame();
+            while poll(Duration::default())? {
+                if let Event::Key(key_code) = read()? {
+                    match key_code.code {
+                        KeyCode::Esc => {
+                            play_goodbye_song(&mut audio);
+                            break 'gameloop;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            // Draw & render
+            let _ = render_tx.send(curr_frame);
+            sleep(Duration::from_millis(1));
+        }
+
+        drop(render_tx);
+        render_handler.join().unwrap();
+    }
     // audio.wait(); // Block until sounds welcome sound finishes playing
 
     // Killing the app and terminating the program
@@ -119,7 +131,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn play_goodbye_song(audio: &mut Audio){
+fn play_goodbye_song(audio: &mut Audio) {
     audio.play("goodbye");
     audio.wait();
 }
